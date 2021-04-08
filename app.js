a282f9ee1ec915b019d122bc42e63f76342f148e
berolso <insta11@hotmail.com>
2021-04-07 18:20:43 -0700

    custom error classes and error handling

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
 const express = require("express");
-const morgan = require('morgan')
+const morgan = require("morgan");
+
+// custom error class extensions
+const { NotFoundError } = require("./expressError");
 
 const authRoutes = require("./routes/auth");
 
 const app = express();
 
 app.use(express.json());
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 app.use("/auth", authRoutes);
 
-app.use('/',(req,res,next)=>{
-  return res.send('hi')
-})
+app.use("/", (req, res, next) => {
+  return res.send("hi");
+});
+
+// handle 404 page not found errors.
+// no matching routes were found
+app.use((req, res, next) => {
+  return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. all errors sent via next(e) end up here */
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV !== "test") console.error(err.stack);
+  const status = err.status || 500;
+  const message = err.message;
+
+  return res.status(status).json({
+    error: { message, status },
+  });
+});
 
 module.exports = app;
---
+++ expressError.js
@@ -0,0 +1,45 @@
+// extend default Errors class
+
+class WaveServerExpressError extends Error {
+  constructor(message, status) {
+    super();
+    this.message = message;
+    this.status = status;
+  }
+}
+
+// Not Found error
+class NotFoundError extends WaveServerExpressError {
+  constructor(message = 'Not Found'){
+    super(message, 404)
+  }
+}
+
+// Unauthorized user error
+class UnauthorizedError extends WaveServerExpressError{
+  constructor(message = 'Unauthorized'){
+    super(message, 401)
+  }
+}
+
+// class BadRequestError
+class BadRequestError extends WaveServerExpressError {
+  constructor(message = 'Bad Request'){
+    super(message, 400)
+  }
+}
+
+// Forbidden Error
+class ForbiddenError extends WaveServerExpressError{
+  constructor(message = 'Bad Request'){
+    super(message, 403)
+  }
+}
+
+module.exports = {
+  WaveServerExpressError,
+  NotFoundError,
+  UnauthorizedError,
+  BadRequestError,
+  ForbiddenError,
+};
